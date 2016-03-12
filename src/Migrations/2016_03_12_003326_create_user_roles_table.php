1d17cdd048cfe11e358942bbfa8a6aac67d22cea
Viacheslav Skydanchuk <nimfus@gmail.com>
2016-03-12 03:16:13 +0100

    V 1.0.3

--- src/Migrations/2016_03_12_003326_create_user_roles_table.php
+++ src/Migrations/2016_03_12_003326_create_user_roles_table.php
@@ -15,7 +15,7 @@ class CreateUserRolesTable extends Migration
         Schema::create('manager_user_roles', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('role_id')->unsigned()->index();
-            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+            $table->foreign('role_id')->references('id')->on('manager_roles')->onDelete('cascade');
             $table->integer('user_id')->unsigned()->index();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
